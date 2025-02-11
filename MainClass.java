73563d489f79799b45c4b850a281406fe4a40474
Amy Tran <amynattran@gmail.com>
2025-02-11 12:09:32 -0800

    playing with gui

--- MainClass.java
+++ MainClass.java
@@ -5,5 +5,7 @@ class MainClasss
         Board gameBoard = new Board(10,15,45);
         System.out.println();
         gameBoard.printBoard();
+        Utils util = new Utils();
+        System.out.println(util.add_nums(3,5));
     }
 }
--- Utils.java
+++ Utils.java
@@ -1,6 +1,33 @@
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 public class Utils {
-    public static void main(String[] args)
+    Utils()
     {
+        Frame frame = new Frame("Example 2");
+
+        // Creating instance of button with label
+        Button button = new Button("Click Here");
+
+        // Setting the position for the button in frame
+        button.setBounds(80, 100, 64, 30);
+
+        // Adding button to the frame
+        frame.add(button);
+
+        // setting size, layout and visibility of frame
+        frame.setSize(300, 300);
+        frame.setLayout(null);
+        frame.setVisible(true);
+
+        // Using WindowListener for closing the window
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e)
+            {
+                System.exit(0);
+            }
+        });
 
     }
 
